51b32286b2d2de68f687d55e8e294a3cdec86640
Lola <lolahef@gmail.com>
2019-04-16 23:19:44 -0400

    Added random gif view

---
+++ src/views/Random.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Gif from '../components/Gif'
+import axios from 'axios'
+
+class Random extends Component {
+  constructor(props) {
+    super(props)
+    this.host        = 'http://api.giphy.com'
+    this.pathRandom  = '/v1/gifs/random'
+    this.state = {
+      gif: false,
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getRandomGif()
+  }
+
+  getRandomGif = () => {
+    axios
+      .get(`${this.host}${this.pathRandom}?api_key=${process.env.REACT_APP_KEY}`)
+      .then(res => this.setState({ gif: res.data.data }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className='random' >
+        <div className='button' onClick={this.getRandomGif}>Show me another!</div>
+        {this.state.gif ? (
+          <Gif gif={this.state.gif} />
+        ) : (
+          <div className='message'>Loading...</div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Random
