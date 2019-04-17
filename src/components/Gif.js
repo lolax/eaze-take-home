9c011d484b21d53bbe3d477aa8e9182c3768100a
Lola <lolahef@gmail.com>
2019-04-17 12:31:38 -0400

    Switched Gif to class component for fav functionality

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,20 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
+import HeartButton from './Heart'
 import ProgressiveImage from 'react-progressive-image'
 
-const Gif = ({ gif }) => (
-  <a href={gif.source ? gif.source : gif.url} key={gif.id}>
-    <ProgressiveImage 
-      src={gif.images.original.url} 
-      placeholder={gif.images.original_still.url}>
-      {src => (
-        <img
-          className='gif' 
-          src={src} 
-          alt={gif.title}
-        />
-      )}
-    </ProgressiveImage>
-  </a>
-)
+class Gif extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      liked: false
+    }
+  }
+
+  componentDidMount() {
+    let favGifs = localStorage.getItem('favs')
+    if (favGifs) {
+      favGifs = JSON.parse(favGifs)
+      this.setState({
+        liked: favGifs[this.props.gif.id] ? true : false
+      })
+      
+    }
+  }
+
+  updateFavs = () => {
+    const { gif } = this.props
+    let favGifs = localStorage.getItem('favs')
+    if (favGifs) {
+      favGifs = JSON.parse(favGifs)
+      this.state.liked ? favGifs[gif.id] = gif : delete favGifs[gif.id]
+      localStorage.setItem('favs', JSON.stringify(favGifs))
+    } else {
+      favGifs = {}
+      this.state.liked ? favGifs[gif.id] = gif : console.log("ruh roh")
+      localStorage.setItem('favs', JSON.stringify(favGifs))
+    }
+  }
   
-  export default Gif
+  favGif = () => {
+    this.setState({ liked: !this.state.liked }, this.updateFavs)
+  }
+
+  render() {
+    return (
+      <div className='gif-container'>
+        <HeartButton 
+          liked={this.state.liked} 
+          favGif={this.favGif} 
+        />
+        <ProgressiveImage 
+          src={this.props.gif.images.original.url} 
+          placeholder={this.props.gif.images.original_still.url}>
+          {src => (
+            <img
+              className='gif' 
+              src={src} 
+              alt={this.props.gif.title}
+            />
+          )}
+        </ProgressiveImage>
+      </div>
+    )
+  }
+}
+  
+export default Gif
