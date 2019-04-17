7eb79b7607eef9646ebd46baf101b403d4013404
Lola <lolahef@gmail.com>
2019-04-17 12:37:46 -0400

    Switched fill & stroke for visibility

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -9,15 +9,14 @@ const HeartButton = ({liked, favGif}) => (
       width="40"
       height="40"
       viewBox="0 0 24 24"
-      stroke={liked ? '#ff3144' : 'white'}
+      stroke={liked ? '#ff3144' : 'rgb(18, 18, 18)'}
       strokeWidth="1"
       strokeLinecap="round"
       strokeLinejoin="round"
       cursor='pointer'
     >
     <path 
-    //   id={id} 
-      fill={liked ? '#ff3144' : 'rgb(18, 18, 18)'}
+      fill={liked ? '#ff3144' : 'white'}
       d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
     </svg>
   </div>
