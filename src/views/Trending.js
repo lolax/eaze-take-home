afad1a3fe0bd0582d85f2af5c283292703daa7c9
Lola <lolahef@gmail.com>
2019-04-16 23:18:10 -0400

    Added view for trending gifs

---
+++ src/views/Trending.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import axios from 'axios'
+
+class Trending extends Component {
+  constructor(props) {
+    super(props)
+    this.host       = 'http://api.giphy.com'
+    this.pathTrend  = '/v1/gifs/trending'
+    this.state = {
+      gifs: [],
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${this.host}${this.pathTrend}?api_key=${process.env.REACT_APP_KEY}`)
+      .then(res => this.setState({ gifs: res.data.data }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className='trending' >
+        <GifList gifs={this.state.gifs} />
+      </div>
+    )
+  }
+}
+
+export default Trending
